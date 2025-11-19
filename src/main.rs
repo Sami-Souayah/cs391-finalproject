9ff470533b498a6baab15fa4674e9de6c09e8e6d
Sami Souayah <souays@bu.edu>
2025-11-19 12:53:23 -0500

    Creating frontend and templates for frontend.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 #[macro_use] extern crate rocket;
+use rocket::form::Form;
+use rocket_dyn_templates::Template;
 
+#[derive(FromForm)]
+struct InputForm {
+    username: String,
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Poopoopeepee"
+fn index() -> Template {
+    Template::render("index", &())
 }
+
+
+
+#[post("/submit", data = "<form_data>")]
+fn submit(form_data: Form<InputForm>) -> String {
+    format!("You entered: {}", form_data.username)
+}
+
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index, submit])
+        .attach(Template::fairing())
 }
